7fc527f880d288807f0e4174b9898c9adca179e9
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 12:38:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(s){
+const cleanedString = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  // Compare the cleaned string with its reverse
+  const reversedString = cleanedString.split("").reverse().join("");
+  return cleanedString === reversedString;
 }
 module.exports = palindrome
